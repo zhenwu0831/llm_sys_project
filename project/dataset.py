cb10d9e5e382cd9e9905441ee244694642446c99
kingofspace0wzz <kingofspacewzz@gmail.com>
2024-04-18 20:03:39 -0400

    updating tokenizer and batch

--- project/dataset.py
+++ project/dataset.py
@@ -9,6 +9,7 @@ from bs4 import BeautifulSoup, NavigableString
 import numpy as np
 from typing import Dict, List, Optional, Iterator, Callable, Union, Tuple
 import os
+import transformers
 
 
 def get_imdb(data_path: str = 'data/imdb.json', split: str = None, silent: bool = False, cache_dir: str = None) -> Dict[str, Dict[str, Union[List[Tuple[int, int]], List[str], str]]]:
@@ -35,6 +36,75 @@ def get_imdb(data_path: str = 'data/imdb.json', split: str = None, silent: bool 
 
     return dataset
 
+def tokenize_batch_element(prompt: str, chosen: str, rejected: str, truncation_mode: str, tokenizer, max_length: int, max_prompt_length: int) -> Dict:
+    """Tokenize a single batch element.
+    
+       At this stage, we don't convert to PyTorch tensors yet; we just handle the truncation
+         in case the prompt + chosen or prompt + rejected responses is/are too long. First
+         we truncate the prompt; if we're still too long, we truncate the chosen/rejected.
+       
+       We also create the labels for the chosen/rejected responses, which are of length equal to
+         the sum of the length of the prompt and the chosen/rejected response, with -100 for the
+         prompt tokens.
+    """
+    chosen_tokens = tokenizer(chosen, add_special_tokens=False)
+    rejected_tokens = tokenizer(rejected, add_special_tokens=False)
+    prompt_tokens = tokenizer(prompt, add_special_tokens=False)
+
+    assert tokenizer.eos_token_id not in prompt_tokens['input_ids'], f"Prompt contains EOS token: {prompt}"
+    assert tokenizer.eos_token_id not in chosen_tokens['input_ids'], f"Chosen response contains EOS token: {chosen}"
+    assert tokenizer.eos_token_id not in rejected_tokens['input_ids'], f"Rejected response contains EOS token: {rejected}"
+
+    chosen_tokens['input_ids'].append(tokenizer.eos_token_id)
+    chosen_tokens['attention_mask'].append(1)
+
+    rejected_tokens['input_ids'].append(tokenizer.eos_token_id)
+    rejected_tokens['attention_mask'].append(1)
+
+    longer_response_length = max(len(chosen_tokens['input_ids']), len(rejected_tokens['input_ids']))
+
+    # if combined sequence is too long, truncate the prompt
+    if len(prompt_tokens['input_ids']) + longer_response_length > max_length:
+        if truncation_mode == 'keep_start':
+            prompt_tokens = {k: v[:max_prompt_length] for k, v in prompt_tokens.items()}
+        elif truncation_mode == 'keep_end':
+            prompt_tokens = {k: v[-max_prompt_length:] for k, v in prompt_tokens.items()}
+        else:
+            raise ValueError(f'Unknown truncation mode: {truncation_mode}')
+
+    # if that's still too long, truncate the response
+    if len(prompt_tokens['input_ids']) + longer_response_length > max_length:
+        chosen_tokens = {k: v[:max_length - max_prompt_length] for k, v in chosen_tokens.items()}
+        rejected_tokens = {k: v[:max_length - max_prompt_length] for k, v in rejected_tokens.items()}
+
+    # Create labels
+    chosen_sequence_tokens = {k: prompt_tokens[k] + chosen_tokens[k] for k in chosen_tokens}
+    rejected_sequence_tokens = {k: prompt_tokens[k] + rejected_tokens[k] for k in rejected_tokens}
+    chosen_sequence_tokens['labels'] = chosen_sequence_tokens['input_ids'][:]
+    chosen_sequence_tokens['labels'][:len(prompt_tokens['input_ids'])] = [-100] * len(prompt_tokens['input_ids'])
+    rejected_sequence_tokens['labels'] = rejected_sequence_tokens['input_ids'][:]
+    rejected_sequence_tokens['labels'][:len(prompt_tokens['input_ids'])] = [-100] * len(prompt_tokens['input_ids'])
+
+    batch = {}
+
+    batch['prompt'] = prompt
+    batch['chosen'] = prompt + chosen
+    batch['rejected'] = prompt + rejected
+    batch['chosen_response_only'] = chosen
+    batch['rejected_response_only'] = rejected
+
+    for k, toks in {'chosen': chosen_sequence_tokens, 'rejected': rejected_sequence_tokens, 'prompt': prompt_tokens}.items():
+        for type_key, tokens in toks.items():
+            if type_key == 'token_type_ids':
+                continue
+            batch[f'{k}_{type_key}'] = tokens
+
+    return batch
+
 if __name__ == '__main__':
     dataset = get_imdb()
     print(dataset['train'][0]['prompt'])
+    
+    tokenizer = transformers.AutoTokenizer.from_pretrained('gpt2')
+    batch = tokenize_batch_element(dataset['train'][0]['prompt'], dataset['train'][0]['chosen'], dataset['train'][0]['rejected'], 'keep_start', tokenizer, 512, 256)
+    
